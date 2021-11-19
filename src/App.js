888d809e8d282b90302dc24ac1812198d7d7c770
minsu-zip <alstn4814@gmail.com>
2021-11-19 22:20:02 +0900

    init : emotion, materaial 환경구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
+import style from '@emotion/styled'
 const App = () => {
-  return <div>test</div>
+  return (
+    <div>
+      <Header>안녕하세요</Header>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  )
 }
+
+const Header = style.h1`
+ text-align : center;
+`
 
 export default App
