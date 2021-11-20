e52524cf41ee6749ea2c3f5114838ea58743f43b
minsu-zip <alstn4814@gmail.com>
2021-11-20 09:01:18 +0900

    feat : axios 셋팅

---
+++ src/apis/api/user.js
@@ -0,0 +1,31 @@
+import { defaultInstance } from '@apis/utils'
+
+// 사용 예시
+// export const getPost = async (selectChannel) => {
+//   try {
+//     const { data } = await defaultInstance.get(`요청주소`)
+//     return data
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// 사용 예시
+// data 필드에 data가 들어가야하면 객체 타입으로 받아온 매게변수를 두번 째 인자에 넣어줌
+// export const postCreate = async (formData) => {
+//   try {
+//     await defaultInstance.post('posts/create', formData)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// 사용 예시
+// axios delete 필드는 data타입이 없기 때문에 직접 data타입을 지정해줘야 한다
+// export const deletePost = async (id) => {
+//   try {
+//     await authInstance.delete('posts/delete', { data: id })
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
---
+++ src/apis/utils/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const BASE_URL =
+  'http://ec2-3-36-132-160.ap-northeast-2.compute.amazonaws.com:8080/'
+
+const axiosApi = (url, options) => {
+  const instance = axios.create({ baseURL: url, ...options })
+  return instance
+}
+
+export const defaultInstance = axiosApi(BASE_URL)
